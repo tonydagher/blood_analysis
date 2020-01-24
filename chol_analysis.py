ba9e80917ea034834557b855438e4279ee47eb09
Tony Dagher <tydagher@gmail.com>
2020-01-24 12:12:30 -0500

    Added basics for user interface

---
+++ chol_analysis.py
@@ -0,0 +1,11 @@
+def interface():
+    print("Cholesterol Calculator")
+    print("Options: ")
+    print("   9 - Quit")
+    choice = input("Enter your option: ")
+    if choice == '9':
+        return
+
+
+if __name__ == "__main__":
+    interface()
